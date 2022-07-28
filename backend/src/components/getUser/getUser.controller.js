6505f53d1d2032349481b965988b8fc466df4f27
Leonardo <castroleonardo@live.cl>
2022-07-27 20:41:56 -0400

    arregladas las respuestas de error

--- backend/src/components/getUser/getUser.controller.js
+++ backend/src/components/getUser/getUser.controller.js
@@ -5,7 +5,7 @@ async function getUserController(req, res) {
         const response = await getUserModule(req);
         res.status(200).send(response);
     } catch (error) {
-        throw error;
+        res.status(500).send(error.message);
     }
 }
 
