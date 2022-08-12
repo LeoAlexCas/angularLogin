00fd783430117fa42a947d9eb9a128560528d18f
Leonardo <castroleonardo@live.cl>
2022-08-11 21:30:52 -0400

    creando compoenentes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './compoments/signin/signin.component';
+import { SignupComponent } from './compoments/signup/signup.component';
+import { InventoryComponent } from './compoments/inventory/inventory.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/inventory',
+    pathMatch: 'full'
+  },
+  {
+    path: 'signin',
+    component: SigninComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: 'inventory',
+    component: InventoryComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
